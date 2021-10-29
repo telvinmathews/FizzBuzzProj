9727a84f85c50b7ed2e4faa16a8b50424585b67e
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-10-29 00:00:00 -0500

    created fizz buzz application

--- fizz_buzz/Program.cs
+++ fizz_buzz/Program.cs
@@ -6,7 +6,34 @@ namespace fizz_buzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create a for loop
+            for (int index = 0; index <= 100; index++)
+            {
+                if (index % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                if (index % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                if (index % 3 == 0 && index % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+            }
+            //start loop at 0
+            //contiue loop until 100
+            //icrement by 1
+
+            //use % to check if num is divisible by 3
+            //print 'fizz'
+
+            //check if num is divisible by 5
+            //print 'buzz'
+
+            //check if number is divisible by 3 and 5
+            //print 'fizzbuzz'
         }
     }
 }
