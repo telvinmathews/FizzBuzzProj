b6b5fd60758df8bbe85a98f831f0b7998d288d66
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-10-28 23:58:58 -0500

    first commit

---
+++ fizz_buzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fizz_buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
